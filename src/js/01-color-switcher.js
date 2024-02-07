7a2eb52fc8a02d6c6c90953f3ca171de311c7463
Pisarski Roman <romanooo84@gmail.com>
2024-02-07 14:05:44 +0100

    changes

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,38 @@
+// wyszukiwanie body
+let body = document.querySelector('body')
+// dodanie klasy do body
+body.classList.add("color")
+// nasłuchowanie body
+body.addEventListener('click', start)
+//deklaracja zmiennych
+let timerId;
+let startButton;
+
+// funkcja zmiany wyboru koloru
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+//funkcja uruchamiająca zmianę tła
+function start(event) {
+    event.preventDefault();
+    // spradzanie nazwy przycisku
+    const switchName = event.target.textContent.toLowerCase()
+    if (switchName === 'start') {
+        startButton = event.target
+        //zablokowanie przysciku start
+        startButton.disabled = true; 
+        //uruchoimienie fukcji bgColor po 1sek.
+        //bgColor zmienia color tła body i odblokowuje przycisk start
+            timerId = setTimeout(bgColor => {
+            body.style.backgroundColor = getRandomHexColor();
+            startButton.disabled = false;
+        }, 1000);
+    } else if (switchName === 'stop') {
+        //usunięcie oczekiwania na wykonanie funkcji
+        clearInterval(timerId);
+        //odblokowanie przycisku start
+        startButton.disabled = false;
+    }
+
+}
